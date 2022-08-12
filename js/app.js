29587273dffa114103284343ad90b2fa2e98d531
capacho <ecapacho@misena.edu.co>
2022-08-12 17:31:29 -0500

    js bootstrap

--- js/app.js
+++ js/app.js
@@ -9,20 +9,54 @@ form.addEventListener("submit",function(e){
     let lista = document.getElementById("list-product");
 
     let div = document.createElement("div");
+    div.classList.add("row")
+    div.classList.add('mb-1')
     div.innerHTML = `
-    <div class="card text-center row">
-    <div class="card-body col-11">
-        <b>Descripcion:</b> ${descripcion.value}
-        <b>Cantidad:</b> ${Cantidad.value}
-        <b>Precio:</b> ${precio.value}
+    <div class="card text-center col-md-11">
+        <div class="card-body">
+            <b>Descripcion:</b> ${descripcion.value}
+            <b>Cantidad:</b> ${Cantidad.value}
+            <b>Precio:</b> ${precio.value}
+        </div>
     </div>
-    <div class="cold 12"> <a href="#" 
-        class="btn btn-danger"
-        name="delete"> BORRAR</a>
-    </div>
-</div>
+    <button
+
+    class= "btn btn-danger text-center col-md-1"
+    name="delete">
+    <i class="fa-solid fa-trash-can"></i>
+    </button>
     `;
-    lista.appendChild(div);
 
-
+    if( descripcion.value!="" && precio.value!="" && Cantidad.value!=""){
+        lista.appendChild(div);
+        mensaje("El Mensaje ha sido Agregado", 'success');
+        form.reset();
+    }
+    
 });
+let borrar = document.querySelector(".row")
+borrar.addEventListener("click",function(e){
+    if(e.target.name === "delete"){
+        e.target.parentElement.remove();
+         mensaje("El Mensaje ha sido Eliminado", 'danger');
+         form.reset();
+       
+    }
+})
+
+const mensaje = function(msj, color){
+    container = document.querySelector(".container");
+    app = document.querySelector("#app");
+    div = document.createElement("div");
+    
+    div.innerHTML =`
+    <div class="alert alert-${color} mt-5">${msj}</div>
+    `;
+
+    container.insertBefore(div ,app);
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+
+
+}
